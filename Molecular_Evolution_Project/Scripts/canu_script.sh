5c4e26d7e6b40ecabc135e576560be24b7ae6eb9
Karl Nyrén <kallejohan211@hotmail.com>
2019-04-04 15:13:27 +0200

    Added correct SBATCH parsing

--- Molecular_Evolution_Project/Scripts/canu_script.sh
+++ Molecular_Evolution_Project/Scripts/canu_script.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/bash
 
-SBATCH
-#SBATCH
-#SBATCH
-#SBATCH
-#SBATCH
-#SBATCH
-#SBATCH
-#-A g2019003
-#-p core
-#-n 1
-#-t 05:30:00
-#-J Genome_Assembly_Karl_Nyren
-#--mail-type=ALL
-#--mail-user karl.nyren.6523@student.uu.se
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 05:30:00
+#SBATCH -J Genome_Assembly_Karl_Nyren
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user karl.nyren.6523@student.uu.se
 
 module load bioinfo-tools
 module load canu
