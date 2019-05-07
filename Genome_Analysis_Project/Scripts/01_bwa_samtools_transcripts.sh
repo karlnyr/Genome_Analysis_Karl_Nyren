ec37d430ae473c94f3a7524f58043af41d829a9c
Karl Nyrén <kallejohan211@hotmail.com>
2019-05-07 17:02:10 +0200

    Added BH

--- Genome_Analysis_Project/Scripts/01_bwa_samtools_transcripts.sh
+++ Genome_Analysis_Project/Scripts/01_bwa_samtools_transcripts.sh
@@ -13,7 +13,7 @@ module load bwa/0.7.8
 module load samtools/1.9
 
 RNA_SERUM='RNA-Seq_Serum'
-RNA_BH='RNA-Seq_Seqrum'
+RNA_BH='RNA-Seq_BH'
 ASSEMBLY_DIR='/home/karlnyr/Genome_Analysis/Genome_Assembly/pilon/Corrected_Assembly_Pilon_v1.contigs.fasta'
 INPUTDIR='/home/karlnyr/Genome_Analysis/raw_data/transcriptomics_data'
 OUTDIR='/home/karlnyr/Genome_Analysis/Mapping'
@@ -34,5 +34,19 @@ for FILE in $INPUTDIR/$RNA_SERUM/trim*pass_1*;
         $READ_1 $READ_2 \
         | \
         command samtools sort \
-        -o $OUTDIR/$READ_PAIR_NAME
+        -o $OUTDIR/$RNA_SERUM/$READ_PAIR_NAME
+done;
+
+for FILE in $INPUTDIR/$RNA_BH/trim*pass_1*;
+    do
+        READ_PAIR_NAME=`basename $FILE | cut -d "_" -f 3`
+        READ_1=$INPUTDIR/$RNA_BH/$FILE_PRE$READ_PAIR_NAME$FILE_EXT_1
+        READ_2=$INPUTDIR/$RNA_BH/$FILE_PRE$READ_PAIR_NAME$FILE_EXT_2
+        command bwa mem \
+        -M \
+        $ASSEMBLY_DIR \
+        $READ_1 $READ_2 \
+        | \
+        command samtools sort \
+        -o $OUTDIR/$RNA_BH/$READ_PAIR_NAME
 done;
