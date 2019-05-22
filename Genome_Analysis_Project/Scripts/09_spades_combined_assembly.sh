3d3e34c9f69598dc75265ea8e02f7edba62c681b
Karl Nyrén <kallejohan211@hotmail.com>
2019-05-22 08:42:36 +0200

    has to be a one liner, changed from paired-end to forward and reverse reads

--- Genome_Analysis_Project/Scripts/09_spades_combined_assembly.sh
+++ Genome_Analysis_Project/Scripts/09_spades_combined_assembly.sh
@@ -14,12 +14,8 @@ module load spades/3.9.0
 ILLUMINA_READS_DIR='/home/karlnyr/Genome_Analysis/raw_data/genomics_data/Illumina/'
 NANOPORE_READS_DIR='/home/karlnyr/Genome_Analysis/raw_data/genomics_data/Nanopore/E745_all.fasta.gz'
 OLD_ASSEMBLY_DIR='/home/karlnyr/Genome_Analysis/Genome_Assembly/canu_assembly/pilon/Corrected_Assembly_Pilon_v1.contigs.fasta'
-OUTPUT_DIR='/home/karlnyr/Genome_Analysis/Genome_Assembly/spades_combined_assembly'
+OUTPUT_DIR='/home/karlnyr/Genome_Analysis/Genome_Assembly/spades_combined_assembly/combined_assembly_220519'
 READ_1_EXT='*1_clean.fq.gz'
 READ_2_EXT='*2_clean.fq.gz'
 
-spades.py \
-    --pe<#>-1 $ILLUMINA_READS_DIR$READ_1_EXT --pe<#>-2 $ILLUMINA_READS_DIR$READ_2_EXT \
-    --nanopore $NANOPORE_READS_DIR \
-    --untrusted-contigs $OLD_ASSEMBLY_DIR \
-    -o $OUTPUT_DIR
+spades.py -1 $ILLUMINA_READS_DIR$READ_1_EXT -2 $ILLUMINA_READS_DIR$READ_2_EXT --nanopore $NANOPORE_READS_DIR --untrusted-contigs $OLD_ASSEMBLY_DIR -o $OUTPUT_DIR
