0767752c4d8d5b218fbaae55154d37eece1c7415
Karl Nyrén <kallejohan211@hotmail.com>
2019-05-23 09:58:37 +0200

    failed to load prokka

--- Genome_Analysis_Project/Scripts/07_prokka.sh
+++ Genome_Analysis_Project/Scripts/07_prokka.sh
@@ -8,6 +8,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user karl.nyren.6523@student.uu.se
 
+module load bioinfo-tools
+module load prokka/1.12-12547ca
+
 OUTDIR='/home/karlnyr/Genome_Analysis/Annotation/DNA/Spades_Genome_Assembly'
 PREFIX='annotated_spades_220519'
 ASSEMBLY_PATH='/home/karlnyr/Genome_Analysis/Genome_Assembly/spades_combined_assembly/combined_assembly_220519/contigs.fasta'
