1620672cb29d53ba2d902a874399cf4ff685f16e
Karl Nyrén <kallejohan211@hotmail.com>
2019-05-27 16:15:04 +0200

    new script for indexing alignment files with samtools.

---
+++ Genome_Analysis_Project/Scripts/11_indexing_samtools.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 03:30:00
+#SBATCH -J INDEXING_SAMTOOLS
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user karl.nyren.6523@student.uu.se
+
+module load bioinfo-tools
+module load samtools/1.9
+
+RNA_Seq_BH='RNA-Seq_BH'
+RNA_Seq_Serum='RNA-Seq_Serum'
+
+input_dir='/home/karlnyr/Genome_Analysis/Mapping/canu_assembly_alignments/'
+suffix_sorted='.sorted.bam'
+suffix_index=$suffix_sorted'.bai'
+
+for FILE in $input_dir/$RNA_Seq_Serum/*;
+    do
+        command samtools sort -O bam $FILE -o $input_dir$RNA_Seq_Serum$FILE$suffix -T $input_dir$RNA_Seq_Serum/'tmp'$FILE
+        command samtools index -b $FILE$suffix_sorted $input_dir$RNA_Seq_Serum$FILE$suffix_index
+    done
+
+for FILE in $input_dir/$RNA_Seq_BH/*;
+    do
+        command samtools sort -O bam $FILE -o $input_dir$RNA_Seq_BH$FILE$suffix -T $input_dir$RNA_Seq_BH/'tmp'$FILE
+        command samtools index -b $FILE$suffix_sorted $input_dir$RNA_Seq_BH$FILE$suffix_index
+    done
